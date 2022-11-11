f90d1fc21ec56607ec1cc36767ca6a3de860e68c
Erik Henrique <erikhrodrigues@gmail.com>
2022-11-11 06:20:34 -0300

    Traduzindo e comentando código

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+//selecionando botões
 let btnAvaliacao = document.getElementsByClassName("avaliacao__selecao__item");
 let btnSubmit = document.getElementById("avaliacaoBtn");
+
+//Array com resultado
 let nota = [];
 
+//Capturando todos botões relacionados a nota final
 Array.from(btnAvaliacao).forEach(b => {
     b.addEventListener("click", () => {
         if(nota.length === 2 ) {
@@ -13,24 +17,29 @@ Array.from(btnAvaliacao).forEach(b => {
     })
 })
 
+//Imprimindo resultado na tela
 btnSubmit.addEventListener("click", () => {
+    
+    //Organizando array do resultado sempre do menor para o maior
     if(nota[0] > nota [1]){
         nota.reverse()
     }
-    
+    //Removendo tela inicial
     let avaliacaoInicial = document.querySelector(".avaliacao")
     avaliacaoInicial.classList.add("sumir")
 
+    //Exibindo tela final
     let avaliacaoFinal = document.querySelector(".avaliacao__final")
     avaliacaoFinal.style.display = "flex";
 
+    //Imprimindo resultado
     let res = document.querySelector(".res");
     if(nota.length === 2){
-        res.innerHTML = `You selected ${nota[0]} out of ${nota[1]}`
+        res.innerHTML = `Você selecionou de ${nota[0]} a ${nota[1]}`
     } else {
-        res.innerHTML = `You selected ${nota[0]}`
+        res.innerHTML = `Você selecionou ${nota[0]}`
     }
-    
+
 })
 
 
